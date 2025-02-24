c40ef2d8b901cec1cbec52b5af6ede5ff764cb20
Your Name <ayan@lynxlabshq.com>
2025-02-23 21:50:13 -0800

    Configure hovering code actions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,7 +24,9 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.gopls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
--- vim-options.lua
+++ vim-options.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.g.mapleader = "<Space>"
 vim.keymap.set('i', 'jk', "<Esc>", {}) -- easier escape
 
@@ -10,3 +12,4 @@ vim.keymap.set('n', "<C-j>", "<C-w>j", {})
 vim.keymap.set('n', "<C-h>", "<C-w>h", {})
 vim.keymap.set('n', "<C-l>", "<C-w>l", {})
 vim.keymap.set('n', "<C-l>", "<C-w>l", {})
+
