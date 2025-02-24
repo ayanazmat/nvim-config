f093fbe6a4ea19cd4913431076853e7493f269dc
Your Name <ayan@lynxlabshq.com>
2025-02-24 00:50:28 -0800

    Update readme + format on save

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Instructions
 
+Install "JetBrainsMono Nerd Font" from [nerdfonts](https://www.nerdfonts.com/font-downloads) (Needed for neo-tree icons)
+```bash
+unzip JetBrainsMono.zip -d JetBrainsMono
+cp -vf ./JetBrainsMono/*.ttf ~/Library/Fonts
+```
+Confirm sure the font is installed on your computer. On macs you can check in the Font Book app.
+
 Install neovim:
 ```bash
 brew install neovim
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,26 +1,38 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-      sources = {
-        -- lua
-        null_ls.builtins.formatting.stylua,
+		null_ls.setup({
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
+			sources = {
+				-- lua
+				null_ls.builtins.formatting.stylua,
 
-        -- ruby
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+				-- ruby
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.rubocop,
 
-        -- golang
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.gofmt,
+				-- golang
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.gofmt,
 
-        -- js/ts
-        null_ls.builtins.formatting.prettierd,
-      },
-    })
+				-- js/ts
+				null_ls.builtins.formatting.prettierd,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
