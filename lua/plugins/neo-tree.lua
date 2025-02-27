3e1c790f02813da7bcb05a827c34a8827d9d441c
Ayan Azmat <ayan@lunafinancialgroup.co>
2025-02-27 11:14:53 -0800

    Fix neo-tree keys + current file following

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,5 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      mappings = {},
-    },
-    window = {
-      mappings = {},
-    },
-  },
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -16,6 +8,44 @@ return {
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+      window = {
+        position = "left",
+        width = 40,
+        mappings = {
+          ["h"] = function(state)
+            local node = state.tree:get_node()
+            if (node.type == "directory" or node:has_children()) and node:is_expanded() then
+              state.commands.toggle_node(state)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+            end
+          end,
+          ["l"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" or node:has_children() then
+              if not node:is_expanded() then
+                state.commands.toggle_node(state)
+              else
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+              end
+            else
+              require("neo-tree.sources.filesystem.commands").open(state)
+            end
+          end,
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    })
     vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle left<Cr>", {})
   end,
 }
